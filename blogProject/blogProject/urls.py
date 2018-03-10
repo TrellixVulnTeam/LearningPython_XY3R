8a31cbc605528bc21467a62538c1ff9ffb373d56
Val Do <valeri.kharitonashvili1@gmail.com>
2018-03-10 13:40:43 +0400

    urls

--- blogProject/blogProject/urls.py
+++ blogProject/blogProject/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+# from django.urls import path
 
-# urlpatterns = [
-#     url(r'^admin/', include(admin.site.urls)),
-#     url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
-# ]
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    # path('admin/', admin.site.urls),
+    url(r'^blog/', include("blog.urls", namespace="blog", app_name="blog")),
+]
